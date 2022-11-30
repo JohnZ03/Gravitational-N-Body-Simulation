a8837af6c837fbb3bdf7532c111e2004e5e3d2b2
Celene <hsuan_ling@hotmail.com>
2022-11-30 16:10:49 -0500

    - added time program run;

--- run.cpp
+++ run.cpp
@@ -1,18 +1,20 @@
 #include "main.cpp"
+#include <chrono>
 
 int step;
 long double steps, endtime;
 
 int main(){
-    
+    //g++ -o run run.cpp
     double start_time, end_time;
-    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    printf("Program is running .... \n");
     System sys;
     sys.SetStartTime(18000);
     sys.timestep = 10; //1
     sys.LoadFile("results/solar_system.start");
     sys.SetOutToFile("results/output_solar_system.csv",1);
-    endtime = 190000000000;
+    endtime = 190000;
     steps = endtime/sys.timestep;
     
     for(step=0;step<steps;step++){
@@ -20,7 +22,9 @@ int main(){
         sys.Step();
     }
     
-//end_time = getElapsedTime();
-    printf("Code took %lf seconds \n", end_time - start_time);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
     
 }
