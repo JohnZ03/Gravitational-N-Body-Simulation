fb84c6095f74d3fb8758f1cb46529c02529bc0fe
Celene <hsuan_ling@hotmail.com>
2022-12-02 17:26:00 -0500

    - update README instructions;

---
+++ run omp.cpp
@@ -0,0 +1,32 @@
+#include "main_openmp.cpp"
+#include <chrono>
+
+int step;
+long double steps, endtime;
+
+int main(){
+    //g++ -o run run.cpp
+    double start_time, end_time;
+    printf("Program is running .... \n");
+    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    System sys;
+    sys.SetStartTime(18000);
+    sys.timestep = 1; //1
+    sys.LoadFile("results/full_solar_system.start");
+    sys.SetOutToFile("results/output_full_solar_system_omp_long.csv",1);
+    //endtime = 19000;
+    endtime = 28000;
+    steps = endtime/sys.timestep;
+    
+    for(step=0;step<steps;step++){
+        sys.Output();
+        sys.Step();
+    }
+    
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+    
+}
