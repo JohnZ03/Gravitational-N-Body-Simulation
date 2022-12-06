702fc9b38a79078d848b4330132918971e881ce9
Celene <hsuan_ling@hotmail.com>
2022-12-06 06:20:49 -0500

    Update mainOmp.cpp

--- mainOmp.cpp
+++ mainOmp.cpp
@@ -374,13 +374,13 @@ public:
 		next_timestep = 9999999999;
         
         
-        #pragma omp parallel for num_threads(32)
+        //#pragma omp parallel for num_threads(32)
         //#pragma omp parallel for shared() \
         private( r_0, v_0, a_0, r_p, v_p, r_rel, v_rel, r_dot_v_relative, abs_r_rel, a_comp, a_dot_comp_1, a_dot_comp_2, a, a_dot)
-		for (std::pair<std::string, vector<long double>> body_itterator : bodies)
-        //for(int body_i = 0; body_i < bodies.size(); body_i++)
+		//for (std::pair<std::string, vector<long double>> body_itterator : bodies)
+        for(int body_i = 0; body_i < bodies.size(); body_i++)
 		{
-            body_id = body_itterator.first; //bodies[body_i];//
+            body_id = bodies.at(body_i).first;//body_itterator.first; ////
             body_values = body_itterator.second; //bodies[body_i].second;//
 			body_final.clear();
 
@@ -472,10 +472,10 @@ public:
 		if (time_counter == output_rate)
 		{
 			cout << "#" + to_string(time) + "\n";
-			for (std::pair<std::string, vector<long double>> body_itterator : bodies)
-            //for(int body_i = 0; body_i < bodies.size(); body_i++)
+			//for (std::pair<std::string, vector<long double>> body_itterator : bodies)
+            for(int body_i = 0; body_i < bodies.size(); body_i++)
 			{
-                body_id = body_itterator.first;//body_itterator.first; //bodies[body_i];//
+                body_id = bodies.at(body_i).first;//body_itterator.first;//body_itterator.first; //bodies[body_i];//
                 body_values = body_itterator.second;//bodies[body_i].second;//
 				cout << body_id;
 				for (i = 0; i < body_values.size(); i++)
